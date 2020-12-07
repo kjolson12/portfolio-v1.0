9b9205ce9ff635ed741fe349996c25a4e66dadd3
kjolson12 <kobaseball02@gmail.com>
2020-12-07 14:41:43 -0800

    attempt at fixing image height

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Skill = ({ imageSource, skill }) => {
     return (
         <div className='ui grid'>
-            <img alt='logo' src={imageSource} className='ui centered tiny image' />
+            <img
+                alt='logo'
+                src={imageSource}
+                className='ui centered tiny fluid image'
+                style={{ height: 'auto' }}
+            />
             <div className='centered row'>
                 <h3>{skill}</h3>
             </div>
